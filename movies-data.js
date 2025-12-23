f9788bca482ceda9bd53490c39c834917cc06792
Gokul Raja <gokulraja0408@gmail.com>
2025-12-23 21:00:44 +0530

    Add files via upload

---
+++ movies-data.js
@@ -0,0 +1,52 @@
+const movies = {
+  interstellar: {
+    title: "Title: Interstellar",
+    cbfc:"CBFC: U/A (India)",
+    year:"Year: 2014",
+    genre:"Genre: Science Fiction, Adventure, Drama",
+    runtime:"Runtime: 169 minutes (2 hours 49 minutes)",
+    imdb:"IMDb Rating: ⭐ 8.7 / 10",
+    overview: "Overview: A team of explorers travel through a wormhole in space in an attempt to ensure humanity’s survival as Earth becomes increasingly uninhabitable, confronting the limits of love, time, and space." },
+  
+    oppenheimer : {
+  title: "Title: Oppenheimer",
+  cbfc: "CBFC: U/A (India)",
+  year: "Year: 2023",
+  genre: "Genre: Biography, Drama, History",
+  runtime: "Runtime: 180 minutes (3 hours)",
+  imdb: "IMDb Rating: ⭐ 8.2 / 10",
+  overview: "Overview: The story of J. Robert Oppenheimer, the theoretical physicist who led the Manhattan Project and played a key role in developing the atomic bomb during World War II, exploring the moral and political consequences of his work."
+},
+
+  avengersEndgame : {
+  title: "Title: Avengers: Endgame",
+  cbfc: "CBFC: U/A (India)",
+  year: "Year: 2019",
+  genre: "Genre: Action, Adventure, Science Fiction",
+  runtime: "Runtime: 181 minutes (3 hours 1 minute)",
+  imdb: "IMDb Rating: ⭐ 8.4 / 10",
+  overview: "Overview: After the devastating events of Avengers: Infinity War, the Avengers assemble once more to reverse Thanos’s actions and restore balance to the universe."
+},
+
+  msDhoniUntoldStory : {
+  title: "Title: M.S. Dhoni: The Untold Story",
+  cbfc: "CBFC: U (India)",
+  year: "Year: 2016",
+  genre: "Genre: Biography, Drama, Sport",
+  runtime: "Runtime: 190 minutes (3 hours 10 minutes)",
+  imdb: "IMDb Rating: ⭐ 7.9 / 10",
+  overview: "Overview: The life story of Indian cricketer Mahendra Singh Dhoni, from his early struggles and small-town beginnings to becoming one of the most successful captains in the history of Indian cricket."
+},
+   
+avatarTheWayOfWater : {
+  title: "Title: Avatar: The Way of Water",
+  cbfc: "CBFC: U/A (India)",
+  year: "Year: 2022",
+  genre: "Genre: Action, Adventure, Science Fiction",
+  runtime: "Runtime: 192 minutes (3 hours 12 minutes)",
+  imdb: "IMDb Rating: ⭐ 7.6 / 10",
+  overview: "Overview: Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. When a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na’vi race to protect their home."
+},
+
+    
+  };
---
+++ series-data.js
@@ -0,0 +1,83 @@
+const webseries ={
+    moneyHeist : {
+  stitle: "Series : Money Heist",
+  syear: "Year : 2017",
+  sgenre: "Genre: Crime, Thriller, Drama",
+
+  director: "Director: Álex Pina",
+  season:  "Season: 5" ,
+
+  episodes: " Season 1 : 13 episodes, Season 2 : 9 episodes, Season 3 : 8 episodes,Season 4 : 8 episodes,Season 5 : 10 episodes",
+  simdb: "IMDb Rating: ⭐ 8.2 / 10",
+  soverview: "Overview: A criminal mastermind known as the Professor plans the biggest heist in history, targeting the Royal Mint of Spain and later the Bank of Spain, while manipulating the police and leading a team of skilled criminals.",
+  
+} ,
+
+ wednesday : {
+  stitle: "Series : Wednesday",
+  syear: "Year : 2022",
+  sgenre: "Genre: Comedy, Crime, Fantasy",
+
+  director: "Director: Tim Burton",
+  season: "Season: 2",
+
+  episodes: "Season 1 : 8 episodes, Season 2 : 8 episodes",
+  simdb: "IMDb Rating: ⭐ 8.1 / 10",
+  soverview: "Overview: Wednesday Addams is sent to Nevermore Academy, where she attempts to master her emerging psychic abilities, solve a murder mystery, and uncover dark secrets connected to her parents’ past."
+},
+
+breakingBad : {
+  stitle: "Series : Breaking Bad",
+  syear: "Year : 2008",
+  sgenre: "Genre: Crime, Drama, Thriller",
+
+  director: "Director: Vince Gilligan",
+  season: "Season: 5",
+
+  episodes: "Season 1 : 7 episodes, Season 2 : 13 episodes, Season 3 : 13 episodes, Season 4 : 13 episodes, Season 5 : 16 episodes",
+  simdb: "IMDb Rating: ⭐ 9.5 / 10",
+  soverview: "Overview: A high school chemistry teacher diagnosed with terminal cancer turns to manufacturing and selling methamphetamine to secure his family’s future, leading him deeper into the criminal underworld."
+},
+
+squidGame : {
+  stitle: "Series : Squid Game",
+  syear: "Year : 2021",
+  sgenre: "Genre: Action, Drama, Thriller",
+
+  director: "Director: Hwang Dong-hyuk",
+  season: "Season: 2",
+
+  episodes: "Season 1 : 9 episodes, Season 2 : 7 episodes",
+  simdb: "IMDb Rating: ⭐ 8.0 / 10",
+  soverview: "Overview: Hundreds of cash-strapped contestants accept an invitation to compete in children's games for a tempting prize, but the stakes are deadly as they risk their lives for a chance to escape crushing debt."
+},
+
+dark : {
+  stitle: "Series : Dark",
+  syear: "Year : 2017",
+  sgenre: "Genre: Science Fiction, Thriller, Drama",
+
+  director: "Director: Baran bo Odar",
+  season: "Season: 3",
+
+  episodes: "Season 1 : 10 episodes, Season 2 : 8 episodes, Season 3 : 8 episodes",
+  simdb: "IMDb Rating: ⭐ 8.7 / 10",
+  soverview: "Overview: When two children go missing in a small German town, its sinful past, present, and future are exposed as four interconnected families uncover a time-travel conspiracy that spans several generations."
+},
+
+strangerThings : {
+  stitle: "Series : Stranger Things",
+  syear: "Year : 2016",
+  sgenre: "Genre: Science Fiction, Horror, Drama",
+
+  director: "Director: The Duffer Brothers",
+  season: "Season: 5",
+
+  episodes: "Season 1 : 8 episodes, Season 2 : 9 episodes, Season 3 : 8 episodes, Season 4 : 9 episodes, Season 5 : 8 episodes",
+  simdb: "IMDb Rating: ⭐ 8.7 / 10",
+  soverview: "Overview: A group of young friends in the town of Hawkins uncover supernatural mysteries, secret government experiments, and terrifying creatures from another dimension known as the Upside Down."
+},
+
+
+
+};
